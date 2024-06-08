f0eed0d7a4be08f91135a1dffff68e060db49744
Satyaki Mullick <satyakiph@gmail.com>
2024-06-08 14:13:07 +0530

    "Adding a strategy file"

---
+++ strategies.py
@@ -0,0 +1,31 @@
+from helper import *
+
+def momentum_long():
+    pass
+
+def momentum_long_sort():
+    pass
+
+
+def momentum_12_1():
+    pass
+
+
+
+def momentum_12_1_long_sort():
+    pass
+
+
+def reversion_long():
+    pass
+
+
+def reversion_long_sort():
+    pass
+
+
+def seasonal_strategy():
+    pass
+
+def value_strategy():
+    pass
